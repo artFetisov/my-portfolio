f1fe83a48429355544540ec9bdd6eb294b02cb3f
artFetisov <fetishfestoff@mail.ru>
2023-01-16 14:59:57 +0300

    change router

--- src/router/router.data.ts
+++ src/router/router.data.ts
@@ -7,10 +7,11 @@ import {ContactPage} from "../components/pages/contact/ContactPage";
 export interface IRoute {
     path: string
     element: FC
+    index?: boolean
 }
 
 export const routes: IRoute[] = [
-    {path: 'my-portfolio/home', element: HomePage},
+    {path: 'my-portfolio/home', element: HomePage, index: true},
     {path: 'my-portfolio/about', element: AboutPage},
     {path: 'my-portfolio/works', element: PortfolioPage},
     {path: 'my-portfolio/contact', element: ContactPage},
