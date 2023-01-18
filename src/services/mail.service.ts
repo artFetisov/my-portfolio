2d1cbf606b7cc06968c08b7a3a858c48c1842f30
artFetisov <fetishfestoff@mail.ru>
2023-01-18 14:36:05 +0300

    refactor

--- src/services/mail.service.ts
+++ src/services/mail.service.ts
@@ -3,6 +3,6 @@ import {IContactData} from "../types/contacts.types";
 
 export const MailService = {
     sendMail(data: IContactData) {
-        return axiosInstance.post('send-mail', data)
+        return axiosInstance.get('/')
     }
 }
