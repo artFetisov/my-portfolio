24b5ae8bbcbf45e3be8fd3057d69a6819d8e8441
artFetisov <fetishfestoff@mail.ru>
2024-07-07 23:39:19 +0300

    ref

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -6,7 +6,7 @@ import {SocialList} from "./SocialList";
 export const Footer: FC = () => {
     return <footer className={styles.footer}>
         <div className={styles.copyright}>
-            <p>Copyright © 2022</p>
+            <p>Copyright © 2024</p>
         </div>
         <SocialList />
     </footer>
