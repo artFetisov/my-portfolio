6762e7c91107e2afd9a1b7b2105bbc48976c3736
artFetisov <fetishfestoff@mail.ru>
2024-11-15 13:43:40 +0300

    feat: add social network in projects

--- src/data/portfolio.data.ts
+++ src/data/portfolio.data.ts
@@ -1,9 +1,10 @@
-import { IconDefinition, faFirstOrder } from '@fortawesome/free-brands-svg-icons'
+import { IconDefinition } from '@fortawesome/free-brands-svg-icons'
 import { faCircleQuestion } from '@fortawesome/free-regular-svg-icons'
 import { faFileExcel } from '@fortawesome/free-solid-svg-icons'
 import quizImage from '../assets/images/1621568763_18-phonoteka_org-p-kviz-fon-23.jpg'
 import excelImg from '../assets/images/excel-indir-scaled.webp'
-import ozonImg from '../assets/images/6528604949.jpg'
+import socialImg from '../assets/images/social.jpg'
+
 
 export type FilterType = 'react' | 'native' | 'next' | 'fullStack' | 'all'
 
@@ -18,12 +19,12 @@ export interface IPortfolioItem {
 
 export const myWorks: IPortfolioItem[] = [
   {
-    category: ['next', 'fullStack'],
-    title: 'Ozon',
-    description: 'A copy of the Ozon online store',
-    icon: faFirstOrder,
-    photo: ozonImg,
-    link: 'https://ozon-front.vercel.app/',
+    category: ['next'],
+    title: 'Communication platform',
+    description: 'Социальная сеть.',
+    icon: faCircleQuestion,
+    photo: socialImg,
+    link: 'https://project-icn.work',
   },
   {
     category: ['react'],
@@ -36,7 +37,7 @@ export const myWorks: IPortfolioItem[] = [
   {
     category: ['native'],
     title: 'Excel',
-    description: 'This is a simplified version of the excel program in native javascript...',
+    description: 'Это упрощенная версия программы Excel.',
     icon: faFileExcel,
     photo: excelImg,
     link: 'https://excel-new-p6do.vercel.app',
